76de8a8fb775ba3249d16e52990335f107a9c6eb
Richard Vanbergen <rich.vanbergen@gmail.com>
2020-10-18 08:18:47 +0900

    Adding build step.

--- packages/test-2/lib/test-2.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-export default () => {
-    return <a href="https://www.bbc.co.uk">Testing react with a link.</a>
-}
---
+++ packages/test-2/src/TestComponent.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default () => {
+  return <a href="https://www.bbc.co.uk">Testing react with a link.</a>
+}
