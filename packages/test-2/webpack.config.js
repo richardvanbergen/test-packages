17c16e982b0955adf5d71120126c2be007731669
Richard Vanbergen <rich.vanbergen@gmail.com>
2020-10-18 12:41:55 +0900

    Adding missing dependencies and react to externals.

--- packages/test-2/webpack.config.js
+++ packages/test-2/webpack.config.js
@@ -4,9 +4,14 @@ module.exports = {
   entry: './src/TestComponent.jsx',
   output: {
     filename: 'main.js',
-    library: 'test-2',
     libraryTarget: 'commonjs2',
     path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    }
   },
   module: {
     rules: [
@@ -18,5 +23,19 @@ module.exports = {
         }
       }
     ]
+  },
+  externals: {
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM"
+    }
   }
 };
